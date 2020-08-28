e01df4eb100905928c3a74e409c01bea2fe4a754
Юрий Шаповалов <mr.Shapovaloff@gmail.com>
2020-08-28 09:57:21 +0300

    Добавляет мобильную версию

--- build/js/script.js
+++ build/js/script.js
@@ -27,37 +27,3 @@
     });
   });
 })();
-
-// (function () {
-//   var cardsWrapper = document.querySelector('.card__wrapper');
-
-//   var breakpoint = window.matchMedia('(min-width:1280px)');
-//   var mySwiper;
-//   var breakpointChecker = function () {
-//     if (breakpoint.matches === true) {
-//       if (mySwiper) {
-//         mySwiper.destroy(true, true);
-//       }
-//       return;
-//     } else if (breakpoint.matches === false) {
-//       enableSwiper();
-//     }
-//   };
-
-
-//   var enableSwiper = function () {
-//     if (cardsWrapper) {
-//       mySwiper = new window.Swiper(cardsWrapper, {
-//         freeMode: true,
-//         nextEl: '.cards__button--next',
-//         prevEl: '.cards__button--prev',
-//         slidesPerView: 3,
-//         spaceBetween: 0,
-//         // grabCursor: true,
-//       });
-//     }
-//   };
-
-//   breakpoint.addListener(breakpointChecker);
-//   breakpointChecker();
-// })();
